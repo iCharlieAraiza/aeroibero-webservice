abd657eec1aa70c571c7fc906168aa92a6bf8c02
Carlos Araiza <eliptikman1234@gmail.com>
2021-04-18 18:49:13 -0500

    Se crearon los modulos

---
+++ src/main/java/mx/aeroibero/main/reservacion/controller/ReservacionController.java
@@ -0,0 +1,11 @@
+package mx.aeroibero.main.reservacion.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservacion")
+public class ReservacionController {
+
+}
